67f08154a010d21f037d244f8d1bdde70d1c3f72
Wendy Gao <vvenxin@gmail.com>
2017-04-10 22:23:51 -0400

    bird

---
+++ sketch.js
@@ -0,0 +1,85 @@
+var img;
+var video;
+ 
+// A variable for the color we are searching for.
+var trackColor;
+ 
+ function preload() {
+  img = loadImage("pigeon.png");
+}
+ 
+function setup() {
+  createCanvas(640, 480);
+  // devicePixelScaling(false);
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  // The above function actually makes a separate video
+  // element on the page.  The line below hides it since we are
+  // drawing the video to the canvas
+  // video.hide();
+ 
+  // Start off tracking for red
+  trackColor = [150, 50, 70];
+  
+}
+ 
+function draw() {
+ 
+ 
+  // Draw the video
+  image(video, 0, 0);
+ 
+  // We are going to look at the video's pixels
+  video.loadPixels();
+ 
+  // Before we begin searching, the "world record" for closest color is set to a high number that is easy for the first pixel to beat.
+  var worldRecord = 500;
+ 
+  // XY coordinate of closest color
+  var closestX = 0;
+  var closestY = 0;
+ 
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+ 
+      var loc = (x + y * video.width) * 4;
+      //var loc = (x + y * video.width) * 4;
+      // The functions red(), green(), and blue() pull out the three color components from a pixel.
+      var r1 = video.pixels[loc];
+      var g1 = video.pixels[loc + 1];
+      var b1 = video.pixels[loc + 2];
+ 
+      var r2 = trackColor[0];
+      var g2 = trackColor[1];
+      var b2 = trackColor[2];
+ 
+      // Using euclidean distance to compare colors
+      var d = dist(r1, g1, b1, r2, g2, b2); // We are using the dist( ) function to compare the current color with the color we are tracking.
+ 
+      // If current color is more similar to tracked color than
+      // closest color, save current location and current difference
+      if (d < worldRecord) {
+        worldRecord = d;
+        closestX = x;
+        closestY = y;
+      }
+    }
+  }
+ 
+  // We only consider the color found if its color distance is less than 10. 
+  // This threshold of 10 is arbitrary and you can adjust this number depending on how accurate you require the tracking to be.
+  if (worldRecord < 50) {
+    // Draw a circle at the tracked pixel
+    
+    
+  // Displays the image at point (0, height/2) at half size
+    image(img, closestX-40, closestY-75, img.width/8, img.height/8);
+  }
+}
+ 
+ 
+function mousePressed() {
+  // Save color where the mouse is clicked in trackColor variable
+  trackColor = video.get(mouseX, mouseY);
+  console.log(trackColor);
+}
